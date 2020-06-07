365de17250d080d6611ce3bea6a23416f2dfd042
DraghiciC <draghici.constantin.danut@gmail.com>
2020-06-07 16:37:36 +0300

    Signed-off-by: DraghiciC <draghici.constantin.danut@gmail.com> Modul de administrare

---
+++ admin.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<?php
+$mysql = new mysqli (
+	'localhost', // locatia serverului (aici, masina locala)
+	'root',       // numele de cont
+	'',    // parola (atentie, in clar!)
+	'shop'   // baza de date
+	);
+
+// verificam daca am reusit
+if (mysqli_connect_errno()) {
+	die ('Conexiunea a esuat...');
+}
+?>
+<html>
+  <head lang="en">
+  <meta charset="UTF-8">
+  <link href="style_admin.css" rel="stylesheet">
+  </head>
+<body>
+  <label>Use the Select fields if you want to see the query results. For everything else use this one</label>
+    <form action="admin.php" method="post">
+    <input type="text" name="instruction">
+    <input type="submit"><br/>
+  </form>
+    <?php
+    if( isset($_POST['instruction'])){
+    $instruction = $_POST["instruction"];
+    if (!($rez = $mysql->query ($instruction))) {
+                    die ('Error on query');
+    }  
+    else echo ($instruction." executed succesfully!");
+    }
+?>
+  <form action="admin.php" method="post">
+    <label><b>Select</b></label>
+    <input type="text" name="fields">
+    <input type="text" name="rest">
+    <input type="submit"><br/>
+  </form>
+      <label>First field is for column names (Example : year,name,age). The second field is for the rest of the select</label>
+
+  <div class="results">
+  
+  <?php 
+  if( isset($_POST['fields'])){
+    $fields = $_POST["fields"];
+    if( isset($_POST['rest'])){
+    $rest = $_POST["rest"];
+    echo ("\"select ".$fields." ".$rest."\" returned :");
+    if (!($rez = $mysql->query ('select '.$fields.' '.$rest))) {
+                    die ('Error on query');
+    } 
+    $columns = explode (",", $fields);
+    while ($inreg = $rez->fetch_assoc()) {
+        foreach ($columns as $item) {
+            echo ("<p class=\"valoare\">".$item." : ".$inreg[$item]. "</p>");
+        }
+        
+        echo "<br>";
+    }
+    } 
+}
+  ?>
+  </div>
+</body>
+</html>
