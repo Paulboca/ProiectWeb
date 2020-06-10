472ebd3b43e264068e24b4c563be5aeb938f6c90
Paulboca <paulboca1999@gmail.com>
2020-06-10 08:07:19 +0300

    Signed-off-by: Paulboca <paulboca1999@gmail.com>

--- footer.php
+++ footer.php
@@ -6,6 +6,8 @@
 				<p> Contact us for any problem. </p>
 				<p> Email: fasione-shop@gmail.com </p>
 				<p> Phone: 0754800865 </p>
+				<a href="rapoarte_diagrama/raport.html">Raport</a>
+				<a href="rapoarte_diagrama/ghid_utilizare.html">Ghid de Utilizare</a>
 		</div>
 	</div>
 	<button id="myBtn"><a href="#top" style="color: black">Top</a></button>
